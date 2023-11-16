83029a760bd46713389dd6a9c7cc85ab292b9b33
Thomas Le Veo <thomasleveo@gmail.com>
2023-11-16 20:05:26 +0100

    Add scroll interaction for revealing skilss ratio

--- app/javascript/controllers/animated_circle_controller.js
+++ app/javascript/controllers/animated_circle_controller.js
@@ -4,6 +4,22 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
+console.log('hello');
+  }
 
+  reveal() {
+    const circles = document.querySelectorAll('circle:nth-child(2)');
+    console.log(circles.length);
+    for (var i = 0; i < circles.length; i++) {
+      var window_height = window.innerHeight;
+      var circle_top = circles[i].getBoundingClientRect().top;
+      var circle_point = 150;
+
+      if (circle_top < window_height - circle_point) {
+        circles[i].classList.add('reveal');
+      } else {
+        circles[i].classList.remove('reveal');
+      }
+    }
   }
 }
