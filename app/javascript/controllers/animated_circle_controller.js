9587c4135cf132860e5a78c94a46aa37ace13ee1
Thomas Le Veo <thomasleveo@gmail.com>
2023-11-16 20:08:46 +0100

    Add comment to scroll interaction process

--- app/javascript/controllers/animated_circle_controller.js
+++ app/javascript/controllers/animated_circle_controller.js
@@ -9,15 +9,21 @@ console.log('hello');
 
   reveal() {
     const circles = document.querySelectorAll('circle:nth-child(2)');
-    console.log(circles.length);
+    // Parcours tous les cercles
     for (var i = 0; i < circles.length; i++) {
+      // Récupère la hauteur de la fenêtre du navigateur
       var window_height = window.innerHeight;
+      // Récupère la position du cercle par rapport au haut du navigateur
       var circle_top = circles[i].getBoundingClientRect().top;
+      // Définit le point de déclenchement de l'animation à partir du bas du navigateur
       var circle_point = 150;
 
+      // Si le cercle est dans la fenêtre du navigateur
       if (circle_top < window_height - circle_point) {
+        // Ajoute la classe reveal
         circles[i].classList.add('reveal');
       } else {
+        // Sinon, retire la classe reveal
         circles[i].classList.remove('reveal');
       }
     }
